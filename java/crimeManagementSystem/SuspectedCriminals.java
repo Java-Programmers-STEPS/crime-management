9b3b04de1bfb6a0841445b0fb5b2972fcfbe4192
Java-Programmers-STEPS <121227713+Java-Programmers-STEPS@users.noreply.github.com>
2023-02-16 16:29:18 +0530

    Add files via upload

---
+++ java/crimeManagementSystem/SuspectedCriminals.java
@@ -0,0 +1,91 @@
+package crimeManagementSystem;
+
+import java.sql.Blob;
+import java.sql.Date;
+
+public class SuspectedCriminals {
+public int id;
+public Blob image,idproof;
+public String fullname,gender,address,policestationname,officerincharge,remark;
+public int height,weight,arrestdate;
+public Date dateofbirth;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public Blob getImage() {
+	return image;
+}
+public void setImage(Blob image) {
+	this.image = image;
+}
+public Blob getIdproof() {
+	return idproof;
+}
+public void setIdproof(Blob idproof) {
+	this.idproof = idproof;
+}
+public String getFullname() {
+	return fullname;
+}
+public void setFullname(String fullname) {
+	this.fullname = fullname;
+}
+public String getGender() {
+	return gender;
+}
+public void setGender(String gender) {
+	this.gender = gender;
+}
+public String getAddress() {
+	return address;
+}
+public void setAddress(String address) {
+	this.address = address;
+}
+public String getPolicestationname() {
+	return policestationname;
+}
+public void setPolicestationname(String policestationname) {
+	this.policestationname = policestationname;
+}
+public String getOfficerincharge() {
+	return officerincharge;
+}
+public void setOfficerincharge(String officerincharge) {
+	this.officerincharge = officerincharge;
+}
+public String getRemark() {
+	return remark;
+}
+public void setRemark(String remark) {
+	this.remark = remark;
+}
+public int getHeight() {
+	return height;
+}
+public void setHeight(int height) {
+	this.height = height;
+}
+public int getWeight() {
+	return weight;
+}
+public void setWeight(int weight) {
+	this.weight = weight;
+}
+public int getArrestdate() {
+	return arrestdate;
+}
+public void setArrestdate(int arrestdate) {
+	this.arrestdate = arrestdate;
+}
+public Date getDateofbirth() {
+	return dateofbirth;
+}
+public void setDateofbirth(Date dateofbirth) {
+	this.dateofbirth = dateofbirth;
+}
+
+}
